fa1d3e97f4e89fb5561b21b22216e2cc3f9c0e9a
Blazej Kanczkowski <blazejkanczkowski@Blazej-Kanczkowski.local>
2025-07-01 14:17:53 +0200

    m

--- jenkins/pipelines/carina.groovy
+++ jenkins/pipelines/carina.groovy
@@ -1,26 +1,27 @@
 pipeline {
     agent any
+
+    tools {
+        maven 'maven'
+    }
+
     parameters {
         string(name: 'suite', defaultValue: 'api', description: 'TestNG suite name')
-        string(name: 'env', defaultValue: 'STAGE', description: 'Test environment')
+        string(name: 'env', defaultValue: 'STAGE', description: 'Environment')
     }
+
     stages {
-        stage('Checkout') {
-            steps {
-                checkout scm
-            }
-        }
         stage('Run Tests') {
             steps {
                 echo "Running tests with suite: ${params.suite} on environment: ${params.env}"
-                sh "mvn clean test -Dsuite=${params.suite} -Denv=${params.env}"
+                sh "${tool 'maven'}/bin/mvn clean test -Dsuite=${params.suite} -Denv=${params.env}"
             }
         }
     }
+
     post {
         always {
-            junit 'target/surefire-reports/*.xml'
-            archiveArtifacts 'target/*.log'
+            junit '**/target/surefire-reports/*.xml'
         }
     }
 }
