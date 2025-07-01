727e082f96820e12d1f644924609ffd01be26f46
Blazej Kanczkowski <blazejkanczkowski@Blazej-Kanczkowski.local>
2025-07-01 14:25:41 +0200

    m

--- jenkins/pipelines/carina.groovy
+++ jenkins/pipelines/carina.groovy
@@ -7,14 +7,22 @@ pipeline {
 
     parameters {
         string(name: 'suite', defaultValue: 'api', description: 'TestNG suite name')
-        string(name: 'env', defaultValue: 'STAGE', description: 'Environment')
+        string(name: 'env', defaultValue: '', description: 'Environment (leave empty if not needed)')
     }
 
     stages {
         stage('Run Tests') {
             steps {
-                echo "Running tests with suite: ${params.suite} on environment: ${params.env}"
-                sh "${tool 'maven'}/bin/mvn clean test -Dsuite=${params.suite} -Denv=${params.env}"
+                echo "Running tests with suite: ${params.suite} and environment: '${params.env}'"
+                script {
+                    def envOption = params.env?.trim()
+                    def cmd = "mvn clean test -Dsuite=${params.suite}"
+                    if (envOption && envOption.length() > 0) {
+                        cmd += " -Denv=${envOption}"
+                    }
+                    echo "Executing command: ${cmd}"
+                    sh "${tool 'maven'}/bin/${cmd}"
+                }
             }
         }
     }
